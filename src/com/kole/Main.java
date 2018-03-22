166684c3d7b3c4a56e28ae39b299a8edd4d97678
Slobodan <slobodan.kosanovic@gmail.com>
2018-03-22 15:51:24 +0100

    Initial commit

---
+++ src/com/kole/Main.java
@@ -0,0 +1,11 @@
+package com.kole;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+
+        byte myByteValue = 10;
+    }
+}
